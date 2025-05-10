70458aa17fc1a8e5193edc124222de3f700ca256
Avinash Ratnam <avinashr@umich.edu>
2025-05-10 17:52:31 -0400

    Added separate cs file for component logic

--- Components/Counter2.razor.cs
+++ Components/Counter2.razor.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components;
 
-public partial class Counter2 {
-    private int var2 = 0;
-    [Parameter]
-    public int var3 { get; set;}=1;
+namespace blazorApp2.Components {
+    public partial class Counter2 { // partial class since it technically merges this with the class in Counter2.razor (@code section)
+        public int currentValue { get; set; } = 0;
+        [Parameter]
+        public int var3 { get; set;} = 1;
 
-    public void clickFunction(){
-        var2+=var3;
+        public void clickFunction(){
+            currentValue+=var3;
+        }
     }
 }
--- obj/Debug/net9.0/blazorApp2.AssemblyInfo.cs
+++ obj/Debug/net9.0/blazorApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazorApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b919c8391f5bd4abbc8c0617236560b6645a44f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8f3129cbe7cb2530c1f9dcf9759c7d824c11897")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazorApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazorApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
